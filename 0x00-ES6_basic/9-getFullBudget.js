c2796beea428f512e9569d2a0cdf843e4540e044
Menor25 <ajirimenor@gmail.com>
2022-12-20 06:32:26 +0100

    Rewrite getFullBudgetObject to use ES6 method properties in the fullBudget object

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,11 +4,11 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(int) {
-      return `$${int}`;
+    getIncomeInDollars(income) {
+      return `$${income}`;
     },
-    getIncomeInEuros(int) {
-      return `${int} euros`;
+    getIncomeInEuros(income) {
+      return `${income} euros`;
     },
   };
 
