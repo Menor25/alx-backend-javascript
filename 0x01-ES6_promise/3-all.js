18e2ba13b1fe16edbb010904933f0a89ee5ef10b
Menor25 <ajirimenor@gmail.com>
2022-12-21 21:05:39 +0100

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,17 @@
+import {uploadPhoto, createUser} from './utils';
+
+export default function handleProfileSignup() {
+    let body;
+    let firstName;
+    let lastName;
+
+    return Promise.all([uploadPhoto(), createUser()]).then((data) => {
+        body = data[0].body;
+        firstName = data[1].firstName;
+        lastName = data[2].lastName;
+
+        console.log(`${body} ${firstName} ${lastName}`);
+    }).catch(() => {
+        console.log('Signup system offline');
+    });
+}
