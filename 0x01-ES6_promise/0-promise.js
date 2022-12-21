d21a68a792f6d1b3768b3e8e3ed631ef022cb46d
Menor25 <ajirimenor@gmail.com>
2022-12-21 20:25:42 +0100

    Return a Promise using this prototype function getResponseFromAPI()

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -3,7 +3,7 @@ export default function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
         const x = 2 * 2
 
-        if (x == 4) {
+        if (x === 4) {
             resolve("Success");
         } else {
             reject(Error("Failed"));
