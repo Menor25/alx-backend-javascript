cc86c42c0bc246b03e29ce4efcb07ee47bfc2351
Menor25 <ajirimenor@gmail.com>
2022-12-22 16:35:24 +0100

    Implement a class named HolbertonCourse with a constructor, getter and setter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class HolbertonCourse {
+    //constructor
+    constructor(name, length, students){
+        this.name = name;
+        this.length = length;
+        this.students = students;
+    }
+
+    //Getter for name
+    get name() {
+        return this._name;
+    }
+    //Set name
+    set name(value) {
+        if (typeof value !== 'string') {
+            throw new TypeError('Name must be a string')
+        }
+        this._name = value;
+    }
+
+    //Getter for length
+    get length() {
+        return this._length;
+    }
+    //Set length
+    set length(value) {
+        if (typeof value !== "number") {
+            throw new TypeError("Length must be a number");
+        }
+        this._length = value;
+    }
+
+    //Get for students
+    get students() {
+        return this._students;
+    }
+}   
