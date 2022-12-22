9e584ec7c3e0b2ffdb84fecd56eeb8b1e9d9aa37
Menor25 <ajirimenor@gmail.com>
2022-12-22 15:49:22 +0100

    Write and export a function named handleProfileSignup. It should accept three arguments firstName (string), lastName (string), and fileName (string). The function should call the two other functions

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,20 @@
-import {signUpUsers} from "./4-user-promise";
-import {uploadPhoto} from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName){
-    return Promise.allSettled([signUpUsers(firstName, lastName), 
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName),
     uploadPhoto(fileName)]).then((data) => {
-        const resArr = [];
-
-        data.forEach((data) => {
-            if (data.status === 'fulfilled') {
-                resArr.push({status: data.status, value: data.value});
-
-            } else {
-                resArr.push({
-                    statis: data.status, value: Error(`${data.reason.message}`),
-                });
-            }
+    const resArr = [];
+    data.forEach((data) => {
+      if (data.status === 'fulfilled') {
+        resArr.push({ status: data.status, value: data.value });
+      } else {
+        resArr.push({
+          status: data.status,
+          value: `Error: ${data.reason.message}`,
         });
-        return resArr;
+      }
     });
+    return resArr;
+  });
 }
