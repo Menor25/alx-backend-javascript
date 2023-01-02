ef4dd5e970dbfb775504340ebe4069041beeea68
Menor25 <ajirimenor@gmail.com>
2023-01-02 22:06:40 +0100

    a function updateStudentGradeByCity that returns an array of students for a specific city with their new grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,9 @@
 const updateStudentGradeByCity = (obj, city, newGrades) => {
     /** Returns an array of students for a specific city with their new grade */
-    const location = obj.filter(result => result.location === city).push({grade: newGrades});
-
-    if (location.some(grade => grade.newGrades === "")) {
-        return location.push({grade: 'N/A'});
-    }
-    const grades = location.map(grade => grade.newGrades);
-    return grades;
+    const location = obj.filter(result => result.location === city).location.map((student) => {
+        const grades = newGrades.filter(x => x.studentId === student.id).map(i => i.grade)[0];
+        const results = grades || 'N/A';
+        return { ...student, results };
+    });
 }
 export default updateStudentGradeByCity;
