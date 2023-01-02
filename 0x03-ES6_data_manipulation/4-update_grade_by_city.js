feda95933a5a3171793b98c01c85aa20a892dde9
Menor25 <ajirimenor@gmail.com>
2023-01-02 22:09:01 +0100

    a function updateStudentGradeByCity that returns an array of students for a specific city with their new grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,11 @@
-const updateStudentGradeByCity = (obj, city, newGrades) => {
-    /** Returns an array of students for a specific city with their new grade */
-    const location = obj.filter(result => result.location === city).location.map((student) => {
-        const grades = newGrades.filter(x => x.studentId === student.id).map(i => i.grade)[0];
-        const results = grades || 'N/A';
-        return { ...student, results };
+export default function getStudentsByLocation(array, city, grad) {
+  return array
+    .filter((i) => i.location === city)
+    .map((student) => {
+      const gradeI = grad
+        .filter((i) => i.studentId === student.id)
+        .map((x) => x.grade)[0];
+      const grade = gradeI || 'N/A';
+      return { ...student, grade };
     });
 }
-export default updateStudentGradeByCity;
