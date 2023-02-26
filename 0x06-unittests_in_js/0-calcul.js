cd3d7f7d72368716285ee41355e4fb8703c4fb4d
Menor25 <ajirimenor@gmail.com>
2023-02-26 21:08:04 +0100

    Task one completed including the tests

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,8 +1,7 @@
 function calculateNumber(a, b) {
-    const a = Math.random(a);
-    const b = Math.random(b);
-
-    return a + b;
+    const first_num = Math.round(a);
+    const second_number = Math.round(b);
+    return first_num + second_number;
 }
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,37 +1,34 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-//Describe the test suit
-describe('calculateNumber:', function() {
-    //Describe the test case
-    describe('Calculate to integers:', function() {
-        //Write the test case
-        it('should return 4 when 1 is added to 3', function() {
+describe("calculateNumber function:", function() {
+    describe("Calculates two integers:", function() {
+        it('Returns 4.', function() {
             assert.strictEqual(calculateNumber(1, 3), 4);
         });
     });
 
-    //Describe the test case
-    describe('Calculate integer and float:', function(){
-        //Write the test case
-        it('should return 4 when 1 is add to 2.8:', function(){
-            assert.strictEqual(calculateNumber(1, 2.8), 4);
+    describe("calculates integer and float:", function() {
+        it("returns 5", function() {
+            assert.strictEqual(calculateNumber(1, 3.7), 5);
         });
     });
 
-    //Describe the test suite
-    describe('Calculate two float:', function() {
-        //Write the test case
-        it('should return 6 when 2.6 is added to 2.8:', function(){
-            assert.strictEqual(calculateNumber(2.6, 2.8), 6);
+    describe("calculates two floats w/border line:", function() {
+        it("returns 6", function() {
+            assert.strictEqual(calculateNumber(1.5, 3.7), 6);
         });
     });
 
-    //Describe the test case
-    describe('Calculate two floats that are round down:', function() {
-        //Write the test case
-        it('should return 5 when 2.4 is added tp 3.3', function() {
-            assert.strictEqual(calculateNumber(2.4, 3.3), 5);
+    describe("calculates two floats:", function() {
+        it("returns 5", function() {
+            assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        });
+    });
+
+    describe("calculates two floats rounded down:", function() {
+        it("retuns 4", function() {
+            assert.strictEqual(calculateNumber(1.2, 3.1), 4);
         });
     });
 });
