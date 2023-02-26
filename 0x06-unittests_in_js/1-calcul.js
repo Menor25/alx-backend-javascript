6752fbb7c5e6d2751e4e80d5f8503d8c4924f0b2
Menor25 <ajirimenor@gmail.com>
2023-02-26 21:21:36 +0100

    Upgrade the function you created in the previous task, add a new argument type

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,17 +1,16 @@
 function calculateNumber(type, a, b) {
-    const a = Math.round(a);
-    const b = Math.round(b);
-    
-    if (type == 'SUM'){
-        return a + b;
-    }
-    if (type == 'SUBTRACT') {
-        return a - b;
-    }
+    const first_num = Math.round(a);
+    const second_num = Math.round(b);
 
-    if (type == 'DIVIDE') {
-        if (b === 0) return 'Error';
-        return a / b; 
+    if (type === 'SUM') {
+        return first_num + second_num
+    }
+    if (type === 'SUBTRACT') {
+        return first_num - second_num
+    }
+    if (type === 'DIVIDE') {
+        if (second_num === 0) return 'Error';
+        return first_num / second_num
     }
 }
 
