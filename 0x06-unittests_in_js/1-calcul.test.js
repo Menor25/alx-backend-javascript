990c99d0842c6441a7977ec12d4508954fe24217
Menor25 <ajirimenor@gmail.com>
2023-02-26 21:19:36 +0100

    Upgrade the function you created in the previous task, add a new argument type

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,11 +1,8 @@
 const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
-//Describe the test suite
 describe("calculateNumber function:", function() {
-    //describe the test case
     describe("Calculates addition of two float:", function() {
-        //write the test case
         it('Returns 6.', function() {
             assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
         });
